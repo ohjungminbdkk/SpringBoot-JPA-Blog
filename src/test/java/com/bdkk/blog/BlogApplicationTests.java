c834fa01f5a1b3430d289daa304ca579f6d6e6bc
unknown <bdkk0209@hotmail.com>
2020-07-06 22:35:50 +0900

    환경세팅 완료

---
+++ src/test/java/com/bdkk/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdkk.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
